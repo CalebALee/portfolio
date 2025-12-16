d98c82b1ddd43c0104048cf18c2cba5998574f05
Caleb Lee <calee@cspire.com>
2025-12-15 23:17:54 -0600

    Adding Routing for Beginning Pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const APP_ROUTES: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('../layouts/app-shell/app-shell').then(m => m.AppShell),
+    children: [
+        {
+            path: '',
+            loadChildren: () =>
+            import('../features/home/home.routes').then(m => m.HOME_ROUTES),
+        }
+    ],
+  },
+];
